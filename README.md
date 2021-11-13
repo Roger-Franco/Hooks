7736359d132be602947a506418f3653d42bd2e2e
Roger Franco <rogerfrancogalt@gmail.com>
2021-11-12 23:33:00 -0400

    useState #1

--- README.md
+++ README.md
@@ -68,3 +68,6 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `yarn build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+npx create-react-app hooks
+npm i react-router-dom@5.2.0
--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+import '../../index.css'
 
 const UseState = (props) => {
+  const [count, setCount] = useState(0)
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+              <span className="text">{count}</span>
+              <div>
+              <button className="btn" onClick={() =>setCount(count-1)}>-1</button>
+              <button className="btn" onClick={() =>setCount(count+1)}>+1</button>
+              <button className="btn" onClick={() =>setCount(0)}>0</button>
+              <button className="btn" onClick={() => setCount(current => current +10)}>+10</button>
+              <button className="btn" onClick={() => setCount(count +10)}>+10</button>
+              </div>
+            </div>
+            <SectionTitle title="Exercício #02" />
         </div>
     )
 }
