7b70d7d994863d801a1bf1d9988b33a20f18ebbc
Roger Franco <rogerfrancogalt@gmail.com>
2021-11-18 22:32:07 -0400

    Desafio useReducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,46 +1,97 @@
-import React, { useReducer } from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useReducer } from "react";
+import PageTitle from "../../components/layout/PageTitle";
 
 const initialState = {
   cart: [],
   products: [],
   user: null,
-  number: 0
-}
+  number: 0,
+};
 
+// multiplicar por 7 divir por 25 e transformar o numero para parse (inteiro), numberaddN
 function reducer(state, action) {
-  switch(action.type) {
-    case 'numberAdd2':
-      return {...state, number:state.number + 2}
-    case 'login':
-      return {...state, user: {name: action.payload}}
-      default:
-        return state
+  switch (action.type) {
+    case "numberAdd2":
+      return { ...state, number: state.number + 2 };
+    case "numberMult7":
+      return { ...state, number: state.number * 7 };
+    case "numberDiv25":
+      return { ...state, number: state.number / 25 };
+    case "numberInt":
+      return { ...state, number: parseInt(state.number)};
+    case "numberAddN":
+      return { ...state, number: state.number + action.payload};
+    case "login":
+      return { ...state, user: { name: action.payload } };
+    default:
+      return state;
   }
 }
 
 const UseReducer = (props) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
-            <div className="center">
-              {state.user ? <span className="text">{state.user.name}</span> :
-              <span className="text">Sem usuário</span>
-              }
-              
-              <span className="text">{state.number}</span>
-              <div>
-                <button className="btn" onClick={() => dispatch({type: 'login', payload: 'Maria'})} >Login</button>
-                <button className="btn" onClick={() => dispatch({type: 'numberAdd2'})} >+2</button>
-              </div>
-            </div>
+  return (
+    <div className="UseReducer">
+      <PageTitle
+        title="Hook UseReducer"
+        subtitle="Uma outra forma de ter estado em componentes funcionais!"
+      />
+      <div className="center">
+        {state.user ? (
+          <span className="text">{state.user.name}</span>
+        ) : (
+          <span className="text">Sem usuário</span>
+        )}
+
+        <span className="text">{state.number}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "login", payload: "Maria" })}
+          >
+            Login
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberAdd2" })}
+          >
+            +2
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberMult7" })}
+          >
+            x7
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberDiv25" })}
+          >
+            /25
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberInt" })}
+          >
+            Int
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberAddN", payload: - 9 })}
+          >
+            - 9
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberAddN", payload: + 11 })}
+          >
+            + 11
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default UseReducer
+export default UseReducer;
