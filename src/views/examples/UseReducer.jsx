a82864369069404d6b5106fe64e615e4bcb3a70d
Roger Franco <rogerfrancogalt@gmail.com>
2021-11-19 21:33:54 -0400

    useReducer #02

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,32 +1,9 @@
 import React, { useReducer } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 
-const initialState = {
-  cart: [],
-  products: [],
-  user: null,
-  number: 0,
-};
+import {initialState, reducer} from '../../store'
+import {numberAdd2, login} from '../../store/actions'
 
-// multiplicar por 7 divir por 25 e transformar o numero para parse (inteiro), numberaddN
-function reducer(state, action) {
-  switch (action.type) {
-    case "numberAdd2":
-      return { ...state, number: state.number + 2 };
-    case "numberMult7":
-      return { ...state, number: state.number * 7 };
-    case "numberDiv25":
-      return { ...state, number: state.number / 25 };
-    case "numberInt":
-      return { ...state, number: parseInt(state.number)};
-    case "numberAddN":
-      return { ...state, number: state.number + action.payload};
-    case "login":
-      return { ...state, user: { name: action.payload } };
-    default:
-      return state;
-  }
-}
 
 const UseReducer = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -48,13 +25,13 @@ const UseReducer = (props) => {
         <div>
           <button
             className="btn"
-            onClick={() => dispatch({ type: "login", payload: "Maria" })}
+            onClick={() => login(dispatch, 'João')}
           >
             Login
           </button>
           <button
             className="btn"
-            onClick={() => dispatch({ type: "numberAdd2" })}
+            onClick={() => numberAdd2(dispatch)}
           >
             +2
           </button>
